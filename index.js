39e75c43de0dd4cff263000f8a2fbfd5d9f06053
William Harrison <william@williamdavidharrison.com.au>
2022-10-20 17:44:41 +0800

    change function name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function flip() {
+function res() {
     const results = ["heads", "tails"];
     const res = results[Math.floor(Math.random() * results.length)];
 
     return res;
 }
 
-module.exports = flip;
+module.exports = res;
