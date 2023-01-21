179e63cc97688c165fe6b0dd3a78e360333abc87
William Harrison <william@williamdavidharrison.com.au>
2023-01-21 17:47:43 +0800

    fix typo in deprecation message + add warning

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function result() {
+    console.warn("[WARN] CoinflipJS has been deprecated, please consider using Easy Script (https://easyscript.dev) instead.");
+
     return Math.random() >= 0.5 ? "heads" : "tails";
 }
 
